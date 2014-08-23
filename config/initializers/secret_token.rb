9015677dce796cd2afe0cc627fce6d3046f42e71
ankoran <jrbushcraft@gmail.com>
2014-08-23 14:30:57 -0500

    'w' is not recognized as an internal or external command, operable program or batch file.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Firstapp::Application.config.secret_key_base = '386e5070782a0fa23e9885d112445da80a5916f99c87c26fa0b9da9d9235287c74b54e9668eee83f91b89339fca069e94576ddcc3336929c4c97f39a26dd8611'
